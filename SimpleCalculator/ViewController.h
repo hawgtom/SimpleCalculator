09405d707893cf3eded55bc8be2e48e93d1828e7
[Gowtham] <[hawgtom@gmail.com]>
2015-12-01 12:34:52 +0530

    Updated for Portrait And Landscape
    Simple Calculator App Supports Both Portrait and Landscape

--- SimpleCalculator/ViewController.h
+++ SimpleCalculator/ViewController.h
@@ -9,7 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *calculatorDisplay;
+- (IBAction)numberPressed:(UIButton *)sender;
+- (IBAction)calculationPressed:(id)sender;
+- (IBAction)equalsPressed;
 
-
+@property (nonatomic) BOOL typingNumber;
+@property (nonatomic) int firstNumber;
+@property (nonatomic) int secondNumber;
+@property (nonatomic, copy) NSString *operation;
 @end
 
--- SimpleCalculator/ViewController.m
+++ SimpleCalculator/ViewController.m
@@ -24,4 +24,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)numberPressed:(UIButton *)sender {
+    NSString *number = sender.currentTitle;
+    if (self.typingNumber)
+    {
+        self.calculatorDisplay.text = [self.calculatorDisplay.text stringByAppendingString:number];
+    } else
+    {
+        self.calculatorDisplay.text = number;
+        self.typingNumber = YES;
+    }
+}
+
+- (IBAction)calculationPressed:(id)sender {
+    self.typingNumber = NO;
+    self.firstNumber = [self.calculatorDisplay.text intValue];
+    self.operation = [sender currentTitle];
+}
+
+- (IBAction)equalsPressed {
+    self.typingNumber = NO;
+    self.secondNumber = [self.calculatorDisplay.text intValue];
+    
+    int result = 0;
+    
+    if ([self.operation isEqualToString:@"+"])
+    {
+        result = self.firstNumber + self.secondNumber;
+    }
+    else if ([self.operation isEqualToString:@"-"])
+    {
+        result = self.firstNumber - self.secondNumber;
+    }
+    
+    self.calculatorDisplay.text = [NSString stringWithFormat:@"%d", result];
+}
+
 @end

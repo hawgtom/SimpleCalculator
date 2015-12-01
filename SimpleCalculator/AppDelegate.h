87ad1a6e6046148daaa3b99ab7d7970c8c6c0a2b
[Gowtham] <[hawgtom@gmail.com]>
2015-12-01 11:27:31 +0530

    Initial Commit

---
+++ SimpleCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleCalculator
+//
+//  Created by Gowtham on 01/12/15.
+//  Copyright © 2015 Gowtham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
